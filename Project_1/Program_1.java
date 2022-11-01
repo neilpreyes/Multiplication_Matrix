6fee4cf4c34593504c09b66a59ebf448c839f2f8
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2022-10-31 23:30:53 -0700

    Created Classic Multiplication Matrix method
    Commented assumed parameters for parts 2 and 3 of project in main method

--- Project_1/Program_1.java
+++ Project_1/Program_1.java
@@ -23,11 +23,38 @@ public class Program_1{
 		return tempMatrix;
 	}
 	
+	public static int[][] emptyMatrixValues(int rows){
+		int[][] tempMatrix = new int[rows][rows];
+		
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < rows; j++) {
+				tempMatrix[i][j] = 0; //empty matrix with values of 0
+			}
+		}
+		
+		return tempMatrix;
+	}
+	
+	public static int[][] classicMult(int[][] matrixA, int[][]matrixB, int numEquations){
+		int[][] result = new int[numEquations][numEquations];
+		result = emptyMatrixValues(numEquations);
+		
+		for(int i = 0; i < numEquations; i++) {
+			for(int j = 0; j < numEquations; j++) {
+				for(int k = 0; k < numEquations; k++) {
+					result[i][j] += matrixA[i][k] * matrixB[k][j];
+				}
+			}
+		}
+		
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		
 		final int RUN_TIMES = 20;
 		int numEquations;
-		int[][] matrixA, matrixB;
+		int[][] matrixA, matrixB, result;
 		
 		for(int i = 1; i <= 7; i++) { //based on previous courses my computer can handle up to 2^7 matrice rows/variables
 			numEquations = (int)Math.pow(2, i); //number of equations is based on powers of 2
@@ -35,7 +62,9 @@ public class Program_1{
 			matrixA = randomMatrixValues(numEquations);
 			matrixB = randomMatrixValues(numEquations);
 			
-			
+			result = classicMult(matrixA, matrixB, numEquations);
+			//result = dcMult(matrixA, matrixB, numEquations); //assuming using same parameters
+			//result = strassenMult(matrixA, matrixB, numEquations); //assuming using same parameters
 		}
 		
 		
