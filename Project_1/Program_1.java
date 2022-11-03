1c698f5572325487490a1f4a30849711b3e0c953
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2022-11-03 14:34:51 -0700

    Added Divide and Conquer Method
    Included time complexity calculations in main method

--- Project_1/Program_1.java
+++ Project_1/Program_1.java
@@ -2,7 +2,7 @@ package Project_1;
 /**Neil Patrick Reyes
  * CS 3310 Section 03
  * Professor Young
- * 10/30/2022
+ * 11/02/2022
  */
 
 import java.util.*;
@@ -50,6 +50,84 @@ public class Program_1{
 		return result;
 	}
 	
+	public static int[][] dcMult(int[][] matrixA, int[][]matrixB, int numEquations){
+		int[][] result = new int[numEquations][numEquations];
+		result = emptyMatrixValues(numEquations);
+		
+		int[][] A11 = new int[numEquations/2][numEquations/2];
+		int[][] A12 = new int[numEquations/2][numEquations/2];
+		int[][] A21 = new int[numEquations/2][numEquations/2];
+		int[][] A22 = new int[numEquations/2][numEquations/2];
+		int[][] B11 = new int[numEquations/2][numEquations/2];
+		int[][] B12 = new int[numEquations/2][numEquations/2];
+		int[][] B21 = new int[numEquations/2][numEquations/2];
+		int[][] B22 = new int[numEquations/2][numEquations/2];
+		int[][] C11 = new int[numEquations/2][numEquations/2];
+		int[][] C12 = new int[numEquations/2][numEquations/2];
+		int[][] C21 = new int[numEquations/2][numEquations/2];
+		int[][] C22 = new int[numEquations/2][numEquations/2];
+		
+		
+		splitMatrix(matrixA, A11, 0, 0);
+		splitMatrix(matrixA, A12, 0, numEquations/2);
+		splitMatrix(matrixA, A21, numEquations/2, 0);
+		splitMatrix(matrixA, A22, numEquations/2, numEquations/2);
+		splitMatrix(matrixB, B11, 0, 0);
+		splitMatrix(matrixB, B12, 0, numEquations/2);
+		splitMatrix(matrixB, B21, numEquations/2, 0);
+		splitMatrix(matrixB, B22, numEquations/2, numEquations/2);
+		
+		C11 = addMatrix(classicMult(A11, B11, numEquations/2), classicMult(A12, B21, numEquations/2), numEquations/2);
+		C12 = addMatrix(classicMult(A11, B12, numEquations/2), classicMult(A12, B22, numEquations/2), numEquations/2);
+		C21 = addMatrix(classicMult(A21, B11, numEquations/2), classicMult(A22, B21, numEquations/2), numEquations/2);
+		C21 = addMatrix(classicMult(A21, B12, numEquations/2), classicMult(A22, B22, numEquations/2), numEquations/2);
+		
+		buildMatrix(C11, result, 0, 0);
+		buildMatrix(C12, result, 0, numEquations/2);
+		buildMatrix(C21, result, numEquations/2, 0);
+		buildMatrix(C22, result, numEquations/2, numEquations/2);
+		
+		
+		return result;
+	}
+	
+	public static void splitMatrix(int[][] Matrix, int[][] newMatrix, int row, int column) {
+		int columnNum = column;
+		
+		for(int i = 0; i < newMatrix.length; i++) {
+			for(int j = 0; j < newMatrix.length; j++) {
+				newMatrix[i][j] = Matrix[row][columnNum++];
+			}
+			columnNum = column; //reset column num
+			row++; //follow row to i
+		}
+		
+	}
+	
+	public static int[][] addMatrix(int[][] matrixOne, int[][] matrixTwo, int num){
+		int[][] C = new int[num][num];
+		
+		for(int i = 0; i < num; i++) {
+			for(int j = 0; j < num; j++) {
+				C[i][j] = matrixOne[i][j] + matrixTwo[i][j];
+			}
+		}
+		
+		return C;
+	}
+	
+	public static void buildMatrix(int[][] Matrix, int[][] newMatrix, int row, int column) {
+		int columnNum = column;
+		
+		for(int i = 0; i < Matrix.length; i++) {
+			for(int j = 0; j < Matrix.length; j++) {
+				newMatrix[row][columnNum++] = Matrix[i][j];
+			}
+			columnNum = column; //reset column num
+			row++; //follow row to i
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		final int RUN_TIMES = 20;
@@ -79,12 +157,16 @@ public class Program_1{
 			 * }
 			 */
 			
+			start = System.nanoTime();
+			result = dcMult(matrixA, matrixB, numEquations);
+			timeDC = System.nanoTime();
+			timeDC = timeDC - start;
 			
-			//result = dcMult(matrixA, matrixB, numEquations); //assuming using same parameters
 			//result = strassenMult(matrixA, matrixB, numEquations); //assuming using same parameters
 			
 			System.out.println("Time Evaluations(for " + numEquations + "x" + numEquations + "): ");
 			System.out.println("Classic Multiplication Matrix Method: " + timeCM);
+			System.out.println("Divide & Conquer Multiplication Matrix Method: " + timeDC);
 		}
 		
 		
