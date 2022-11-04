4beae954adac08cec21da0831a6b15205fbee1a6
neilpreyes <98624021+neilpreyes@users.noreply.github.com>
2022-11-03 20:29:47 -0700

    Strassen method

--- Project_1/Program_1.java
+++ Project_1/Program_1.java
@@ -2,7 +2,7 @@ package Project_1;
 /**Neil Patrick Reyes
  * CS 3310 Section 03
  * Professor Young
- * 11/02/2022
+ * 11/03/2022
  */
 
 import java.util.*;
@@ -35,7 +35,7 @@ public class Program_1{
 		return tempMatrix;
 	}
 	
-	public static int[][] classicMult(int[][] matrixA, int[][]matrixB, int numEquations){
+	public static int[][] classicMult(int[][] matrixA, int[][] matrixB, int numEquations){
 		int[][] result = new int[numEquations][numEquations];
 		result = emptyMatrixValues(numEquations);
 		
@@ -50,7 +50,7 @@ public class Program_1{
 		return result;
 	}
 	
-	public static int[][] dcMult(int[][] matrixA, int[][]matrixB, int numEquations){
+	public static int[][] dcMult(int[][] matrixA, int[][] matrixB, int numEquations){
 		int[][] result = new int[numEquations][numEquations];
 		result = emptyMatrixValues(numEquations);
 		
@@ -116,6 +116,18 @@ public class Program_1{
 		return C;
 	}
 	
+	public static int[][] subMatrix(int[][]matrixOne, int[][]matrixTwo, int num){
+int[][] C = new int[num][num];
+		
+		for(int i = 0; i < num; i++) {
+			for(int j = 0; j < num; j++) {
+				C[i][j] = matrixOne[i][j] - matrixTwo[i][j];
+			}
+		}
+		
+		return C;
+	}
+	
 	public static void buildMatrix(int[][] Matrix, int[][] newMatrix, int row, int column) {
 		int columnNum = column;
 		
@@ -128,12 +140,77 @@ public class Program_1{
 		}
 	}
 	
+	public static int[][] strassenMult(int[][] matrixA, int[][]matrixB, int numEquations){
+		int[][] result = new int[numEquations][numEquations];
+		
+		result = strassenRecursion(matrixA, matrixB, result, numEquations);
+		
+		return result;
+	}
+	
+	public static int[][] strassenRecursion(int[][] matrixA, int[][] matrixB, int[][] matrixC, int numEquations) {
+		
+		if(numEquations == 2) {
+			matrixC[0][0] = (matrixA[0][0] * matrixB[0][0]) + (matrixA[0][1] * matrixB[1][0]);
+			matrixC[0][1] = (matrixA[0][0] * matrixB[0][1]) + (matrixA[0][1] * matrixB[1][1]);
+			matrixC[1][0] = (matrixA[1][0] * matrixB[0][0]) + (matrixA[1][1] * matrixB[1][0]);
+			matrixC[1][1] = (matrixA[1][0] * matrixB[0][1]) + (matrixA[1][1] * matrixB[1][1]);
+			return matrixC;
+		}else {
+			int[][] A11 = new int[numEquations/2][numEquations/2];
+			int[][] A12 = new int[numEquations/2][numEquations/2];
+			int[][] A21 = new int[numEquations/2][numEquations/2];
+			int[][] A22 = new int[numEquations/2][numEquations/2];
+			int[][] B11 = new int[numEquations/2][numEquations/2];
+			int[][] B12 = new int[numEquations/2][numEquations/2];
+			int[][] B21 = new int[numEquations/2][numEquations/2];
+			int[][] B22 = new int[numEquations/2][numEquations/2];
+			
+			int[][] P = new int[numEquations/2][numEquations/2];
+			int[][] Q = new int[numEquations/2][numEquations/2];
+			int[][] R = new int[numEquations/2][numEquations/2];
+			int[][] S = new int[numEquations/2][numEquations/2];
+			int[][] T = new int[numEquations/2][numEquations/2];
+			int[][] U = new int[numEquations/2][numEquations/2];
+			int[][] V = new int[numEquations/2][numEquations/2];
+			
+			splitMatrix(matrixA, A11, 0, 0);
+			splitMatrix(matrixA, A12, 0, numEquations/2);
+			splitMatrix(matrixA, A21, numEquations/2, 0);
+			splitMatrix(matrixA, A22, numEquations/2, numEquations/2);
+			splitMatrix(matrixB, B11, 0, 0);
+			splitMatrix(matrixB, B12, 0, numEquations/2);
+			splitMatrix(matrixB, B21, numEquations/2, 0);
+			splitMatrix(matrixB, B22, numEquations/2, numEquations/2);
+			
+			strassenRecursion(addMatrix(A11, A22, A11.length), addMatrix(B11, B22, B11.length), P, P.length);
+			strassenRecursion(addMatrix(A21, A22, A21.length), B11, Q, Q.length);
+			strassenRecursion(A11, subMatrix(B12, B22, B12.length), R, R.length);
+			strassenRecursion(A22, subMatrix(B21, B11, B21.length), S, S.length);
+			strassenRecursion(addMatrix(A11, A12, A11.length), B22, T, T.length);
+			strassenRecursion(subMatrix(A21, A11, A21.length), addMatrix(B11, B12, B11.length), U, U.length);
+			strassenRecursion(subMatrix(A12, A22, A12.length), addMatrix(B21, B22, B21.length), V, V.length);
+			
+			int[][] C11 = addMatrix(subMatrix(addMatrix(P, S, P.length), T, T.length), V, V.length);
+			int[][] C12 = addMatrix(R, T, numEquations/2);
+			int[][] C21 = addMatrix(Q, S, numEquations/2);
+			int[][] C22 = addMatrix(subMatrix(addMatrix(P, R, P.length), Q, Q.length), U, U.length);
+			
+			buildMatrix(C11, matrixC, 0, 0);
+			buildMatrix(C12, matrixC, 0, numEquations/2);
+			buildMatrix(C21, matrixC, numEquations/2, 0);
+			buildMatrix(C22, matrixC, numEquations/2, numEquations/2);
+			
+			return matrixC;
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		final int RUN_TIMES = 20;
 		int numEquations;
 		int[][] matrixA, matrixB, result;
-		long start;
+		long start, end;
 		long timeCM = 0;
 		long timeDC = 0;
 		long timeS = 0;
@@ -141,32 +218,79 @@ public class Program_1{
 		for(int i = 1; i <= 7; i++) { //based on previous courses my computer can handle up to 2^7 matrice rows/variables
 			numEquations = (int)Math.pow(2, i); //number of equations is based on powers of 2
 			
-			matrixA = randomMatrixValues(numEquations);
-			matrixB = randomMatrixValues(numEquations);
+			//iteration
+			for(int j = 1; j <= RUN_TIMES; j++) {
+				matrixA = randomMatrixValues(numEquations);
+				/** Check if output is proper
+				 * System.out.println("Matrix A: ");
+				 * for(int num = 0; num < numEquations; num++){
+				 * 	for(int num2 = 0; num2 < numEquations; num2++){
+				 * 		System.out.print(matrixA[num][num2] + " ");
+				 * 	}
+				 * 	System.out.println();
+				 * }
+				 */
+				matrixB = randomMatrixValues(numEquations);
+				/** Check if output is proper
+				 * System.out.println("Matrix B: ");
+				 * for(int num = 0; num < numEquations; num++){
+				 * 	for(int num2 = 0; num2 < numEquations; num2++){
+				 * 		System.out.print(matrixB[num][num2] + " ");
+				 * 	}
+				 * 	System.out.println();
+				 * }
+				 */
+				
+				start = System.nanoTime();
+				result = classicMult(matrixA, matrixB, numEquations);
+				end = System.nanoTime();
+				timeCM += end - start;
+				/** Check if output is proper
+				 * for(int num = 0; num < numEquations; num++){
+				 * 	for(int num2 = 0; num2 < numEquations; num2++){
+				 * 		System.out.print(result[num][num2] + " ");
+				 * 	}
+				 * 	System.out.println();
+				 * }
+				 */
+				
+				start = System.nanoTime();
+				result = dcMult(matrixA, matrixB, numEquations);
+				end = System.nanoTime();
+				timeDC += end - start;
+				/** Check if output is proper
+				 * for(int num = 0; num < numEquations; num++){
+				 * 	for(int num2 = 0; num2 < numEquations; num2++){
+				 * 		System.out.print(result[num][num2] + " ");
+				 * 	}
+				 * 	System.out.println();
+				 * }
+				 */
+				
+				start = System.nanoTime();
+				result = strassenMult(matrixA, matrixB, numEquations);
+				end = System.nanoTime();
+				timeS += end - start;
+				/** Check if output is proper
+				 * for(int num = 0; num < numEquations; num++){
+				 * 	for(int num2 = 0; num2 < numEquations; num2++){
+				 * 		System.out.print(result[num][num2] + " ");
+				 * 	}
+				 * 	System.out.println();
+				 * }
+				 */
+				
+				
+			}
 			
-			start = System.nanoTime();
-			result = classicMult(matrixA, matrixB, numEquations);
-			timeCM = System.nanoTime();
-			timeCM = timeCM - start;
-			/** Check if output is proper
-			 * for(int num = 0; num < numEquations; num++){
-			 * 	for(int num2 = 0; num2 < numEquations; num2++){
-			 * 		System.out.print(result[num][num2] + " ");
-			 * 	}
-			 * 	System.out.println();
-			 * }
-			 */
+			timeCM = timeCM/RUN_TIMES;
+			timeDC = timeDC/RUN_TIMES;
+			timeS = timeS/RUN_TIMES;
 			
-			start = System.nanoTime();
-			result = dcMult(matrixA, matrixB, numEquations);
-			timeDC = System.nanoTime();
-			timeDC = timeDC - start;
-			
-			//result = strassenMult(matrixA, matrixB, numEquations); //assuming using same parameters
-			
-			System.out.println("Time Evaluations(for " + numEquations + "x" + numEquations + "): ");
+			System.out.println("Average Time Evaluations(for " + numEquations + "x" + numEquations + "): ");
 			System.out.println("Classic Multiplication Matrix Method: " + timeCM);
 			System.out.println("Divide & Conquer Multiplication Matrix Method: " + timeDC);
+			System.out.println("Strassen Multiplication Matrix Method: " + timeS);
 		}
 		
 		
